6634fd930d06ec12dab1866f3bb74b63c01e8f6f
Jeff DiNoto <jeff.dinoto@gmail.com>
2021-01-07 01:16:49 -0500

    Fix hugo extended issues

--- vagrant/bootstrap-hugo.sh
+++ vagrant/bootstrap-hugo.sh
@@ -5,19 +5,22 @@ echo "Bootstraping hugo..."
 ## For Ubuntu 18.04 (bionic) apt-get will install the non-extended version of hugu
 ## we require the extended version, so it must be built from source here
 
-echo "mkdir ~/src"
-mkdir ~/src
+echo "mkdir ~/tmp/hugo"
+mkdir ~/tmp/hugo
 
-echo "cd ~/src/"
-cd ~/src/
+echo "cd ~/tmp/hugo"
+cd ~/tmp/hugo/
 
-echo "git clone https://github.com/gohugoio/hugo.git"
-git clone https://github.com/gohugoio/hugo.git
+echo "wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz"
+wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz
 
-echo "cd hugo"
-cd hugo
+echo "tar -xf hugo_extended_0.80.0_Linux-64bit.tar.gz"
+tar -xf hugo_extended_0.80.0_Linux-64bit.tar.gz
 
-echo "go install --tags extended"
-go install --tags extended
+echo "mv hugo /usr/local/bin/hugo"
+mv hugo /usr/local/bin/hugo
+
+echo "rm -rf ~/tmp/hugo/"
+rm -rf ~/tmp/hugo/
 
 echo "Completed bootstraping hugo"
