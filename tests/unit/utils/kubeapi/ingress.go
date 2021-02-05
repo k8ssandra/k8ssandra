b5fe4843854879aa1e8a1b10ec0c18d6a656cb0f
Jake Robb <jake.robb@datastax.com>
2021-02-05 13:38:07 -0500

    changing file names, and import aliases to all-lowercase per code review feedback

--- tests/unit/utils/kubeapi/ingress.go
+++ tests/unit/utils/kubeapi/ingress.go
@@ -3,11 +3,11 @@ package kubeapi
 import (
 	. "fmt"
 	. "github.com/onsi/gomega"
-	networkingV1 "k8s.io/api/networking/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1beta1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func FindIngressRuleByHttpPath(rules []networkingV1.IngressRule, path string) (*networkingV1.IngressRule, *networkingV1.HTTPIngressPath) {
+func FindIngressRuleByHttpPath(rules []networkingv1.IngressRule, path string) (*networkingv1.IngressRule, *networkingv1.HTTPIngressPath) {
 	for _, ruleCandidate := range rules {
 		for _, pathCandidate := range ruleCandidate.HTTP.Paths {
 			if pathCandidate.Path == path {
@@ -18,12 +18,12 @@ func FindIngressRuleByHttpPath(rules []networkingV1.IngressRule, path string) (*
 	return nil, nil
 }
 
-func VerifyNoRuleWithPath(rules []networkingV1.IngressRule, path string) {
+func VerifyNoRuleWithPath(rules []networkingv1.IngressRule, path string) {
 	authRule, _ := FindIngressRuleByHttpPath(rules, path)
 	ExpectWithOffset(1, authRule).To(BeNil())
 }
 
-func VerifyIngressRule(rules []networkingV1.IngressRule, releaseName, path string, pathType *networkingV1.PathType, host string, port int) {
+func VerifyIngressRule(rules []networkingv1.IngressRule, releaseName, path string, pathType *networkingv1.PathType, host string, port int) {
 	rule, httpPath := FindIngressRuleByHttpPath(rules, path)
 
 	description := Sprintf("rule not found for path %v", path)
