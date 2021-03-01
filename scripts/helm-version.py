5d90cf46133aae53f1702b6c11f63d03076bae88
Michael Burman <michael.burman@datastax.com>
2021-03-01 20:59:21 +0200

    Parameter --dep allows to update the dependency in k8ssandra Chart.yaml (#469)

--- scripts/helm-version.py
+++ scripts/helm-version.py
@@ -36,24 +36,11 @@ class Semver:
 
         raise Exception(F'Invalid input version value: {input_str}')
 
-
-def update_charts(update_func, chart):
-    main_dir = subprocess.run(["git", "rev-parse", "--show-toplevel"], check=True, stdout=subprocess.PIPE).stdout.strip().decode('utf-8')
-
-    if chart == "*":
-        search_path = F'{main_dir}/charts/**/Chart.yaml'
-        for path in glob.glob(search_path, recursive=True):
-
-            if re.match('^.*cass-operator.*', path):
-                continue
-
-            update_chart_version(path, update_func)
-    else:
-        update_chart_version(F'{main_dir}/charts/{chart}/Chart.yaml', update_func)
-
-def update_chart_version(path, update_func):
+def update_chart_version(update_func, chart_name, update_dep):
         yaml = YAML()
         yaml.indent(mapping = 2, sequence=4, offset=2)
+        main_dir = subprocess.run(["git", "rev-parse", "--show-toplevel"], check=True, stdout=subprocess.PIPE).stdout.strip().decode('utf-8')
+        path = F'{main_dir}/charts/{chart_name}/Chart.yaml'
         with open(path) as f:
             chart = yaml.load(f)
 
@@ -65,24 +52,50 @@ def update_chart_version(path, update_func):
             yaml.dump(chart, f)
 
         print(F'Updated {path} to {semver.to_string()}')
+        if chart_name != 'k8ssandra' and update_dep is True:
+            update_dependency(main_dir, chart_name, semver.to_string())
+            update_chart_version(update_func, 'k8ssandra', False)
+
+def update_dependency(main_dir, dependency, target_version):
+    yaml = YAML()
+    yaml.indent(mapping = 2, sequence=4, offset=2)
+    path = F'{main_dir}/charts/k8ssandra/Chart.yaml'
+    # If we update the k8ssandra dependency.. we need to update its version also. Why not do it with the same code as above?
+    with open(path) as f:
+        chart = yaml.load(f)
+
+    for k in chart['dependencies']:
+        if k['name'] == dependency:
+            k['version'] = target_version
+            with open(path, 'w') as f:
+                yaml.dump(chart, f)
+
+            print(F'Updated dependency {dependency} to {target_version}')
+            break
 
 def main():
     parser = argparse.ArgumentParser(description='Update Helm chart versions in k8ssandra project')
     parser.add_argument('--incr', choices=['major', 'minor', 'patch'], help='increase part of semver by one')
     parser.add_argument('--chart', choices=['medusa-operator','k8ssandra-common','reaper-operator','k8ssandra','cass-operator','backup','restore'], help='increase only a single chart')
+    parser.add_argument('--dep', help='update the k8ssandra dependency to newest version also', dest='dep_update', action='store_true')
     args = parser.parse_args()
 
     chart = args.chart
     if not args.chart:
         chart = "*"
 
+    update_dep = False
+
+    if args.dep_update is True:
+        update_dep = True
+
     if args.incr:
         if args.incr == 'major':
-            update_charts(Semver.incr_major, chart)
+            update_chart_version(Semver.incr_major, chart, update_dep)
         elif args.incr == 'minor':
-            update_charts(Semver.incr_minor, chart)
+            update_chart_version(Semver.incr_minor, chart, update_dep)
         elif args.incr == 'patch':
-            update_charts(Semver.incr_patch, chart)
+            update_chart_version(Semver.incr_patch, chart, update_dep)
 
 if __name__ == "__main__":
     main()
