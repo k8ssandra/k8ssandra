ff02fdb67962827aa87e0f6fa606e2c3640ed991
Michael Burman <michael.burman@datastax.com>
2021-03-01 23:30:37 +0200

    Add a script to update operator image tag (#472)

--- scripts/helm-version.py
+++ scripts/helm-version.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import subprocess
 from ruamel.yaml import YAML
-import glob
 import re
 import argparse
+
 
 class Semver:
     def __init__(self, major: int, minor: int, patch: int):
@@ -36,29 +36,32 @@ class Semver:
 
         raise Exception(F'Invalid input version value: {input_str}')
 
+
 def update_chart_version(update_func, chart_name, update_dep):
-        yaml = YAML()
-        yaml.indent(mapping = 2, sequence=4, offset=2)
-        main_dir = subprocess.run(["git", "rev-parse", "--show-toplevel"], check=True, stdout=subprocess.PIPE).stdout.strip().decode('utf-8')
-        path = F'{main_dir}/charts/{chart_name}/Chart.yaml'
-        with open(path) as f:
-            chart = yaml.load(f)
+    yaml = YAML()
+    yaml.indent(mapping=2, sequence=4, offset=2)
+    main_dir = subprocess.run(["git", "rev-parse", "--show-toplevel"],
+                              check=True, stdout=subprocess.PIPE).stdout.strip().decode('utf-8')
+    path = F'{main_dir}/charts/{chart_name}/Chart.yaml'
+    with open(path) as f:
+        chart = yaml.load(f)
 
-        semver = Semver.parse(chart['version'])
-        update_func(semver)
-        chart['version'] = semver.to_string()        
+    semver = Semver.parse(chart['version'])
+    update_func(semver)
+    chart['version'] = semver.to_string()
 
-        with open(path, 'w') as f:
-            yaml.dump(chart, f)
+    with open(path, 'w') as f:
+        yaml.dump(chart, f)
 
-        print(F'Updated {path} to {semver.to_string()}')
-        if chart_name != 'k8ssandra' and update_dep is True:
-            update_dependency(main_dir, chart_name, semver.to_string())
-            update_chart_version(update_func, 'k8ssandra', False)
+    print(F'Updated {path} to {semver.to_string()}')
+    if chart_name != 'k8ssandra' and update_dep is True:
+        update_dependency(main_dir, chart_name, semver.to_string())
+        update_chart_version(update_func, 'k8ssandra', False)
+
 
 def update_dependency(main_dir, dependency, target_version):
     yaml = YAML()
-    yaml.indent(mapping = 2, sequence=4, offset=2)
+    yaml.indent(mapping=2, sequence=4, offset=2)
     path = F'{main_dir}/charts/k8ssandra/Chart.yaml'
     # If we update the k8ssandra dependency.. we need to update its version also. Why not do it with the same code as above?
     with open(path) as f:
@@ -73,11 +76,16 @@ def update_dependency(main_dir, dependency, target_version):
             print(F'Updated dependency {dependency} to {target_version}')
             break
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Update Helm chart versions in k8ssandra project')
-    parser.add_argument('--incr', choices=['major', 'minor', 'patch'], help='increase part of semver by one')
-    parser.add_argument('--chart', choices=['medusa-operator','k8ssandra-common','reaper-operator','k8ssandra','cass-operator','backup','restore'], help='increase only a single chart')
-    parser.add_argument('--dep', help='update the k8ssandra dependency to newest version also', dest='dep_update', action='store_true')
+    parser = argparse.ArgumentParser(
+        description='Update Helm chart versions in k8ssandra project')
+    parser.add_argument(
+        '--incr', choices=['major', 'minor', 'patch'], help='increase part of semver by one')
+    parser.add_argument('--chart', choices=['medusa-operator', 'k8ssandra-common', 'reaper-operator',
+                                            'k8ssandra', 'cass-operator', 'backup', 'restore'], help='target chart to update')
+    parser.add_argument('--dep', help='update the k8ssandra dependency to newest version also',
+                        dest='dep_update', action='store_true')
     args = parser.parse_args()
 
     chart = args.chart
@@ -96,6 +104,7 @@ def main():
             update_chart_version(Semver.incr_minor, chart, update_dep)
         elif args.incr == 'patch':
             update_chart_version(Semver.incr_patch, chart, update_dep)
+
 
 if __name__ == "__main__":
     main()
