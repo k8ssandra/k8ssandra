5f14c0434e56dd7c5e3c7baceabb50a33d3ceacc
John Sanda <john.sanda@gmail.com>
2021-05-24 18:28:46 -0400

    use `kubectl cluster-info dump` to collect artifacts

--- tests/integration/scripts/collect_artifacts.sh
+++ tests/integration/scripts/collect_artifacts.sh
@@ -5,6 +5,8 @@ mkdir -p $ARTIFACTS_DIR
 kubectl get namespaces > $ARTIFACTS_DIR/namespaces.txt
 k8ssandra_ns=$(kubectl get namespaces|grep k8ssandra|cut -d' ' -f1)
 
+kubectl cluster-info dump --namespaces $k8ssandra_ns -o yaml --output-directory $ARTIFACTS_DIR
+
 # List all objects from the k8ssandra namespace
 kubectl get all -n $k8ssandra_ns > $ARTIFACTS_DIR/k8ssandra_all.txt
 
@@ -13,19 +15,19 @@ kubectl describe cassandradatacenter/dc1 -n $k8ssandra_ns > $ARTIFACTS_DIR/cassa
 kubectl get cassandradatacenter/dc1 -o yaml -n $k8ssandra_ns > $ARTIFACTS_DIR/cassandradc_dc1.txt
 
 # Extract logs from the Cassandra pods
-for pod in $(kubectl get pods -n $k8ssandra_ns|grep dc1-default-sts|cut -d' ' -f1); do
-    echo "Storing artifacts for pod $pod..."
-    kubectl logs pod/$pod cassandra -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_cassandra.log || echo "can't extract cassandra log"
-    kubectl logs pod/$pod server-system-logger -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_system_log.log || echo "can't extract server-system-logger log"
-    kubectl logs pod/$pod medusa -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_medusa.log || echo "can't extract medusa logs"
-done
+#for pod in $(kubectl get pods -n $k8ssandra_ns|grep dc1-default-sts|cut -d' ' -f1); do
+#    echo "Storing artifacts for pod $pod..."
+#    kubectl logs pod/$pod cassandra -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_cassandra.log || echo "can't extract cassandra log"
+#    kubectl logs pod/$pod server-system-logger -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_system_log.log || echo "can't extract server-system-logger log"
+#    kubectl logs pod/$pod medusa -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_medusa.log || echo "can't extract medusa logs"
+#done
 
-# Extract data from all pods
-for pod in $(kubectl get pods -n $k8ssandra_ns|cut -d' ' -f1); do
-    echo "Extracting description for pod $pod..."
-    kubectl get pod/$pod -o yaml -n $k8ssandra_ns > $ARTIFACTS_DIR/pod_${pod}.txt
-    kubectl describe pod/$pod -n $k8ssandra_ns > $ARTIFACTS_DIR/pod_${pod}_describe.txt
-done
+## Extract data from all pods
+#for pod in $(kubectl get pods -n $k8ssandra_ns|cut -d' ' -f1); do
+#    echo "Extracting description for pod $pod..."
+#    kubectl get pod/$pod -o yaml -n $k8ssandra_ns > $ARTIFACTS_DIR/pod_${pod}.txt
+#    kubectl describe pod/$pod -n $k8ssandra_ns > $ARTIFACTS_DIR/pod_${pod}_describe.txt
+#done
 
 # Extract backup information
 for backup in $(kubectl get CassandraBackup -n $k8ssandra_ns|grep -v "NAME"|cut -d' ' -f1); do
@@ -41,8 +43,8 @@ for restore in $(kubectl get CassandraRestore -n $k8ssandra_ns|grep -v "NAME"|cu
     kubectl get CassandraBackup/$restore -o yaml -n $k8ssandra_ns > $ARTIFACTS_DIR/restore_${restore}.txt
 done
 
-# Extract Reaper logs
-for pod in $(kubectl get pods -n $k8ssandra_ns|grep reaper|cut -d' ' -f1); do
-    echo "Storing reaper log artifacts for pod $pod..."
-    kubectl logs pod/$pod -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}.log || echo "can't extract reaper log"
-done
+## Extract Reaper logs
+#for pod in $(kubectl get pods -n $k8ssandra_ns|grep reaper|cut -d' ' -f1); do
+#    echo "Storing reaper log artifacts for pod $pod..."
+#    kubectl logs pod/$pod -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}.log || echo "can't extract reaper log"
+#done
