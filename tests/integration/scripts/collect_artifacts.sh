dca8848541cd3446f173e8ae5396377c769a2b64
Michael Burman <michael.burman@datastax.com>
2021-05-27 12:07:19 +0300

    Modify collect_artifacts.sh to get cass-operator logs

--- tests/integration/scripts/collect_artifacts.sh
+++ tests/integration/scripts/collect_artifacts.sh
@@ -13,11 +13,17 @@ kubectl describe cassandradatacenter/dc1 -n $k8ssandra_ns > $ARTIFACTS_DIR/cassa
 kubectl get cassandradatacenter/dc1 -o yaml -n $k8ssandra_ns > $ARTIFACTS_DIR/cassandradc_dc1.txt
 
 # Extract logs from the Cassandra pods
-for pod in $(kubectl get pods -n $k8ssandra_ns|grep dc1-default-sts|cut -d' ' -f1); do
+for pod in $(kubectl get pods -n $k8ssandra_ns -l app.kubernetes.io/managed-by=cass-operator|cut -d' ' -f1); do
     echo "Storing artifacts for pod $pod..."
     kubectl logs pod/$pod cassandra -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_cassandra.log || echo "can't extract cassandra log"
     kubectl logs pod/$pod server-system-logger -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_system_log.log || echo "can't extract server-system-logger log"
     kubectl logs pod/$pod medusa -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}_medusa.log || echo "can't extract medusa logs"
+done
+
+# Extract logs from the cassandra-operator pod
+for pod in $(kubectl get pods -n $k8ssandra_ns -l app.kubernetes.io/name=cass-operator|cut -d' ' -f1); do
+    echo "Storing artifacts for pod $pod..."
+    kubectl logs pod/$pod -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}.log || echo "can't extract cass-operator log"
 done
 
 # Extract data from all pods
@@ -42,7 +48,7 @@ for restore in $(kubectl get CassandraRestore -n $k8ssandra_ns|grep -v "NAME"|cu
 done
 
 # Extract Reaper logs
-for pod in $(kubectl get pods -n $k8ssandra_ns|grep reaper|cut -d' ' -f1); do
+for pod in $(kubectl get pods -n $k8ssandra_ns -l app.kubernetes.io/managed-by=reaper-operator|cut -d' ' -f1); do
     echo "Storing reaper log artifacts for pod $pod..."
     kubectl logs pod/$pod -n $k8ssandra_ns > $ARTIFACTS_DIR/${pod}.log || echo "can't extract reaper log"
 done
