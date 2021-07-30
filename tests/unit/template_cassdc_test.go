f0786e77499e904978bf972061e114732e0ba3ab
Jeff DiNoto <jeff.dinoto@gmail.com>
2021-07-30 14:53:31 -0400

    Merge the 4.0.0 GA related commits from 1.3 into main (#1014)

--- tests/unit/template_cassdc_test.go
+++ tests/unit/template_cassdc_test.go
@@ -3,10 +3,11 @@ package unit_test
 import (
 	"encoding/json"
 	"fmt"
-	helmUtils "github.com/k8ssandra/k8ssandra/tests/unit/utils/helm"
-	"github.com/k8ssandra/k8ssandra/tests/unit/utils/kubeapi"
 	"path/filepath"
 	"strconv"
+
+	helmUtils "github.com/k8ssandra/k8ssandra/tests/unit/utils/helm"
+	"github.com/k8ssandra/k8ssandra/tests/unit/utils/kubeapi"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
 	cassdcv1beta1 "github.com/k8ssandra/cass-operator/operator/pkg/apis/cassandra/v1beta1"
@@ -1302,11 +1303,11 @@ var _ = Describe("Verify CassandraDatacenter template", func() {
 
 	Context("when configuring the Cassandra version and/or image", func() {
 		cassandraVersionImageMap := map[string]string{
-			"3.11.7":  "k8ssandra/cass-management-api:3.11.7-v0.1.26",
-			"3.11.8":  "k8ssandra/cass-management-api:3.11.8-v0.1.26",
-			"3.11.9":  "k8ssandra/cass-management-api:3.11.9-v0.1.26",
-			"3.11.10": "k8ssandra/cass-management-api:3.11.10-v0.1.26",
-			"4.0.0":   "k8ssandra/cass-management-api:4.0.0-v0.1.26",
+			"3.11.7":  "k8ssandra/cass-management-api:3.11.7-v0.1.27",
+			"3.11.8":  "k8ssandra/cass-management-api:3.11.8-v0.1.27",
+			"3.11.9":  "k8ssandra/cass-management-api:3.11.9-v0.1.27",
+			"3.11.10": "k8ssandra/cass-management-api:3.11.10-v0.1.27",
+			"4.0.0":   "k8ssandra/cass-management-api:4.0.0-v0.1.27",
 		}
 
 		It("using the default version", func() {
@@ -1317,7 +1318,7 @@ var _ = Describe("Verify CassandraDatacenter template", func() {
 			Expect(renderTemplate(options)).To(Succeed())
 
 			Expect(cassdc.Spec.ServerVersion).To(Equal("3.11.10"))
-			Expect(cassdc.Spec.ServerImage).To(Equal("k8ssandra/cass-management-api:3.11.10-v0.1.26"))
+			Expect(cassdc.Spec.ServerImage).To(Equal("k8ssandra/cass-management-api:3.11.10-v0.1.27"))
 		})
 
 		It("using 3.11.7", func() {
