1cd2d41f5bbf4c86a33187cc9d7393de6a1576bb
Jeff DiNoto <jeff.dinoto@gmail.com>
2022-01-19 20:39:02 -0500

    Setup for v2 branch

--- .github/scripts/env_variables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "YQ_VERSION=v4.6.3" >> $GITHUB_ENV
-echo "HELM_VERSION=v3.5.3" >> $GITHUB_ENV
-echo "GO_VERSION=1.15" >> $GITHUB_ENV
-echo "GOROOT=/usr/local/go1.15" >> $GITHUB_ENV
