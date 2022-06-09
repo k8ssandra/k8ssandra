28b26e8586305b18cdcaba4e13371b9183ea73a0
Michael Burman <michael.burman@datastax.com>
2022-06-09 13:29:31 +0300

    Remove labels test code for cass-operator

--- tests/unit/template_labels_test.go
+++ tests/unit/template_labels_test.go
@@ -80,45 +80,6 @@ var _ = Describe("Verify k8ssandra and dependent template labels", func() {
 		})
 	})
 
-	// Context("by rendering cass-operator templates having k8ssandra common labels", func() {
-	// 	It("using default options", func() {
-
-	// 		path, err := filepath.Abs(CassOperatorChartsPath)
-	// 		Expect(err).To(BeNil())
-	// 		localChartsPath = path
-
-	// 		options := &helm.Options{
-	// 			KubectlOptions: defaultKubeCtlOptions,
-	// 			SetValues: map[string]string{
-	// 				// This test will fail if these are enabled since cert-manager is not installed
-	// 				"admissionWebhooks.enabled": "false",
-	// 			},
-	// 		}
-
-	// 		// Verify required labels for ea. template
-	// 		requiredLabels := GetRequiredLabels(localChartsPath)
-	// 		templates := GetTemplates(localChartsPath)
-	// 		for _, template := range templates {
-
-	// 			var k8ssandraTemplates map[string]interface{}
-	// 			idx := strings.Index(template, "templates")
-
-	// 			templateOutput, err := helm.RenderTemplateE(GinkgoT(), options,
-	// 				localChartsPath, HelmReleaseName, []string{filepath.Join(".", template[idx:])})
-
-	// 			Expect(err).To(BeNil())
-	// 			Expect(templateOutput).ToNot(BeEmpty())
-
-	// 			Expect(helm.UnmarshalK8SYamlE(GinkgoT(), templateOutput, &k8ssandraTemplates)).To(BeNil())
-	// 			Expect(k8ssandraTemplates["metadata"]).ToNot(BeNil())
-
-	// 			for k, v := range requiredLabels {
-	// 				Expect(k8ssandraTemplates["metadata"].(map[string]interface{})["labels"]).To(HaveKeyWithValue(k, v))
-	// 			}
-	// 		}
-	// 	})
-	// })
-
 	Context("by rendering medusa-operator templates having k8ssandra common labels", func() {
 		It("using default options", func() {
 
