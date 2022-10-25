9cfe35db67fcd134157bc8a5a629797a84dc75c4
Michael Burman <michael.burman@datastax.com>
2022-10-25 17:24:45 +0300

    Use ServiceMonitor Capabilities API instead for check

--- tests/unit/utils/helm/helm.go
+++ tests/unit/utils/helm/helm.go
@@ -35,7 +35,7 @@ func RenderAndUnmarshall(templatePath string, options *helm.Options, helmChartPa
 
 	renderedOutput, renderErr := helm.RenderTemplateE(
 		GinkgoT(), options, helmChartPath, HelmReleaseName,
-		[]string{templatePath}, "--api-versions", "cert-manager.io/v1",
+		[]string{templatePath}, "--api-versions=cert-manager.io/v1", "--api-versions=monitoring.coreos.com/v1",
 	)
 
 	// Use CLI option: --rendertmpl={"dir":"/tmp/foo", "name":"my-test.yaml"}
